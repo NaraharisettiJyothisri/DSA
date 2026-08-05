605288f6c320281e07b397b60c2e604e1ab4cd7d
NaraharisettiJyothisri <naraharisettijyothisri2006@gmail.com>
2026-08-05 12:29:10 +0530

    Fixed Basics folder

---
+++ Basics/Countdigit.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public static int countDigit(int n) {
+
+        int count = 0;
+
+        while (n > 0) {
+            n = n / 10;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 12345;
+
+        System.out.println(countDigit(n));
+    }
+}
